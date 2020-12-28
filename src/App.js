e31d890e1afc279ddc160eeca700f9b1160878e3
Bottin Antoine <Toine@MacBook-Pro-de-Bottin.local>
2020-12-28 13:41:21 +0100

    splash page ok

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Splash from './Splash'
+import Home from './Home'
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Splash}/>
+        <Route path="/Home" exact component={Home}/>
+
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import './CSS/Home.css'
+
+
+function Home() {
+  return (
+      <div>
+          COUCOU
+      </div>
+  );
+}
+
+export default Home;
