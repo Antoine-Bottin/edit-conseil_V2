1c80daad49ac8a3f9266422031f4ef1af9cb778e
Bottin Antoine <Toine@MacBook-Pro-de-Bottin.local>
2020-12-31 08:02:49 +0100

    burgerMenu ready

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,24 @@
 import React from 'react';
 //import './CSS/Home.css'
+import { slide as Menu } from 'react-burger-menu'
+import './CSS/Home.css'
+
 
 
 function Home() {
   return (
       <div>
-          COUCOU
+        <Menu   right 
+                width={'15%'}
+                
+                >
+            <a id="edit" className="menu-item" href="/">Edit.</a>
+            <a id="prestations" className="menu-item" href="/prestations">Prestations</a>
+            <a id="commentaires" className="menu-item" href="/commentary">Commentaires</a>
+            <a id="prestations" className="menu-item" href="/prestations">About</a>
+            <a id="prestations" className="menu-item" href="/prestations">About</a>
+            <a id="contact" className="menu-item" href="/contact">Contact</a>
+        </Menu>
       </div>
   );
 }
